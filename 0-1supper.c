de31a6244f4d7ffeceb8b78ab1b7142536de7f17
DanieFaraday <117766722+DanieFaraday@users.noreply.github.com>
2022-12-16 22:20:34 +0100

    Update 0-1supper.c

--- 0-1supper.c
+++ 0-1supper.c
@@ -1,1 +1,15 @@
-
+#include  "main.h"
+/**
+* _isupeer - checks for uppercase character
+* @c : character to check
+* Return: 0 Always
+*/
+int _isupper(int c)
+  
+{
+         if (c >= 'A'  && c <=  'Z')
+                 return  (1);
+         else
+                 return  (0);
+  
+}
